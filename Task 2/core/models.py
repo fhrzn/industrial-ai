36293450875e443784a05fadccfdbd3013a3ce71
affand20 <fahri.lafa@gmail.com>
2022-11-23 19:24:17 +0300

    update params for labels dict

--- Task 2/core/models.py
+++ Task 2/core/models.py
@@ -4,18 +4,27 @@ from transformers import AutoFeatureExtractor, ResNetForImageClassification
 
 def get_bert_model(checkpoint, label2id=None, id2label=None, num_labels=24, problem_type='multi_label_classification'):
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-    model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=num_labels, problem_type=problem_type)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        checkpoint, num_labels=num_labels, 
+        problem_type=problem_type, id2label=id2label,
+        label2id=label2id)
 
     return tokenizer, model
 
 def get_resnet_model(checkpoint, label2id=None, id2label=None, num_labels=24, problem_type='multi_label_classification'):
     tokenizer = AutoFeatureExtractor.from_pretrained(checkpoint)
-    model = ResNetForImageClassification.from_pretrained(checkpoint, num_labels=num_labels, problem_type=problem_type)
+    model = ResNetForImageClassification.from_pretrained(
+        checkpoint, num_labels=num_labels, 
+        problem_type=problem_type, id2label=id2label,
+        label2id=label2id)
 
     return tokenizer, model
 
 def get_vit_model(checkpoint, label2id=None, id2label=None, num_labels=24, problem_type='multi_label_classification'):
     tokenizer = ViTFeatureExtractor.from_pretrained(checkpoint)
-    model = ViTForImageClassification.from_pretrained(checkpoint, num_labels=num_labels, problem_type=problem_type)
+    model = ViTForImageClassification.from_pretrained(
+        checkpoint, num_labels=num_labels, 
+        problem_type=problem_type, id2label=id2label,
+        label2id=label2id)
 
     return tokenizer, model
